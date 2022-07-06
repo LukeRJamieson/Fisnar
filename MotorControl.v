861eac9dc5807ae6863e700535185879e19e6bcd
Luke Jamieson <luke.jamieson@gmail.com>
2022-07-06 20:50:30 +1000

    Motor Control

--- MotorControl.v
+++ MotorControl.v
@@ -3,87 +3,176 @@ module MotorControl
 input clk,
 input rst,
 // Software Control Signals
-input EN,
-input init_home,
-input [18:0] target,
+input Enable,
+input Home,
+input [31:0] Target,
 // Hardware Physical Signals
 input LS,
 input AWOff,
+
 // Acceleration Parameters
-input [25:0] clock_speed,
-input [25:0] max_speed, //800,000 for XY, 320,000 of Z
-input [25:0] min_speed,
-input [25:0] accel_steps, // how many steps between min speed and max speed
+input [31:0] max_speed_count, // This is the counter value for running at maximum speed. eg max speed is 50000 (50mm/s). Using the 50MHz clock this will be (50,000,000/2)/2000 = 500
+input [31:0] min_speed_count, // This is the counter value for running at minimum speed. eg min speed is 2000 (2mm/s). Using the 50MHz clock this will be (50,000,000/2)/2000 = 12500
+input [31:0] accel_steps, // This is how many steps the motors should travel between the min and max speed. For this example we let it go from 2mm/s to 50mm/s in 10mm (10000 steps) . 
+input [31:0] speed_increment, // (maxspeed-minspeed)/accel_steps (let the HPS work this out)
+
 // Movement and Range Parameters
-input CW_polarity, // 0: CW = +1, 1: CW = -1
-input [18:0] home_location, // after homing / at limit switch, what is the location
-input [18:0] lower_limit,
-input [18:0] upper_limit,
+input CW_polarity, // 0: CW = +1, 1: CW = -1 (X and Z = 0; Y = 1)
+input [31:0] home_location, // after homing / at limit switch, what is the location
+input [31:0] lower_limit,
+input [31:0] upper_limit,
 
 // Outputs
 output CW,
 output CCW,
-output reg [18:0] location,
-output targetAtLocation,
+output reg [31:0] Location,
+output reg Done //flag
 
-output [25:0] DEBUG_currentspeed,
-output [25:0] DEBUG_speedincrement
 );
 
 // Acceleration Control (Dynamic Clock)
-reg step;
-reg [18:0] start_location;
-reg [18:0] end_location; // Also the target
-reg [19:0] counter;
-reg [19:0] speed_divider;
-reg [25:0] current_speed;
-wire [25:0] speed_increment;
-assign speed_increment = (max_speed - min_speed) / accel_steps;
+reg Step;
+reg [31:0] counter;
+reg [31:0] speed_count;
+reg DIR_CW;
+reg DIR_CCW;
 
 initial
 begin
-	start_location = 0;
+	speed_count = min_speed_count;
 end
  
 
 always@(posedge clk) //TODO
 begin
-	if(EN && !AWOff)
-		begin
-		end
-		
-	// state machine
-
-	speed_divider <= clock_speed/current_speed; // divide by zero bug here if min_speed = 0
-	counter = counter + 20'b1;
-	if (counter >= speed_divider)
+	counter = counter + 32'b1;
+	if (Enable && (counter >= speed_count))
 	begin
-		
 		counter <= 0;
-		step <= ~step;
+		Step <= ~Step;
 	end
-	
-	
 end
 
-reg homed;
-always@(posedge step)
+always@(posedge Step)
 begin
-	if(start_location == location)
-		current_speed <= min_speed;
-	else if (current_speed < max_speed)
-		current_speed <= current_speed + speed_increment;
-	else
-		current_speed <= max_speed;
+	if(Home && !LS)
+		begin
+			Location <= 0;
+			DIR_CW <= 0;
+			DIR_CCW <= 1;
+			Done <= 0;
+			speed_count <= min_speed_count;
+		end
 
-
+	else if(Home && LS)
+		begin
+			Location <= home_location;
+			DIR_CW <= 0;
+			DIR_CCW <= 0;
+			Done <= 1;
+			speed_count <= min_speed_count;
+		end
 	
-	location <= location + 1;
+///////////////// Counting up to target///////////////////////////
+	else if(Enable && (Location < Target))
+	begin
+		Location <= Location + 32'b1;
+		if(CW_polarity)
+		begin
+			DIR_CW <= 0;
+			DIR_CCW <= 1;
+		end
+		else
+		begin
+			DIR_CW <= 1;
+			DIR_CCW <= 0;
+		end
+		Done <= 0;
+
+/////////////////////// Speed Control //////////////////////////////
+		if(((Location + accel_steps) > Target) && (speed_count < min_speed_count)) //((Location - Target) < accel_steps) && (speed_count < min_speed_count)
+		begin
+			speed_count <= speed_count + speed_increment;
+		end
+		else if((Location + accel_steps) > Target  && speed_count >= min_speed_count) //(Location - Target) < accel_steps
+		begin
+			speed_count <= min_speed_count;
+		end
+		else if(speed_count <= max_speed_count)
+		begin
+			speed_count <= max_speed_count;
+		end
+		else if(speed_count > max_speed_count)
+		begin
+			speed_count <= speed_count - speed_increment;
+		end
+		else
+		begin
+			speed_count <= min_speed_count;
+		end
+///////////////////////////////////////////////////////////////////			
+	end
+///////////////// Counting down to target///////////////////////////
+	else if(Enable && (Location > Target))
+	begin
+		Location <= Location - 32'b1;
+		if(CW_polarity)
+		begin
+			DIR_CW <= 1;
+			DIR_CCW <= 0;
+		end
+		else
+		begin
+			DIR_CW <= 0;
+			DIR_CCW <= 1;
+		end
+		Done <= 0;
+
+/////////////////////// Speed Control //////////////////////////////
+		if(((Target + accel_steps) > Location) && (speed_count < min_speed_count))
+		begin
+			speed_count <= speed_count + speed_increment; // Slow Down
+		end
+		else if((Target + accel_steps) > Location && speed_count >= min_speed_count)
+		begin
+			speed_count <= min_speed_count;
+		end
+		else if(speed_count <= max_speed_count)
+		begin
+			speed_count <= max_speed_count;			// Maintain Speed
+		end
+		else if(speed_count > max_speed_count)
+		begin
+			speed_count <= speed_count - speed_increment; // Speed up
+		end
+		else
+		begin
+			speed_count <= min_speed_count;				// Run at Min
+		end
+///////////////////////////////////////////////////////////////////	
+	end
+
+	else if(Enable && (Location == Target))
+	begin
+		Location <= Location;
+		DIR_CW <= 0;
+		DIR_CCW <= 0;
+		Done <= 1;
+		speed_count <= min_speed_count;
+	end
+
+	else
+	begin
+		Location <= Location;
+		DIR_CW <= 0;
+		DIR_CCW <= 0;
+		Done <= 0;
+		speed_count <= min_speed_count;
+	end
 end
 
-// DEBUG
-assign CW = step;
-assign DEBUG_currentspeed = current_speed;
-assign DEBUG_speedincrement = speed_increment;
+assign CW = Step & DIR_CW;
+assign CCW = Step & DIR_CCW;
+
 
 endmodule
